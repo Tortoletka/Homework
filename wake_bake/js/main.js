fedf97b255c5f242cb920d0e326a7ae0070c6030
Tortoletka <Tortoletka1306@gmail.com>
2025-07-07 02:35:57 +0300

    Add files via upload

--- wake_bake/js/main.js
+++ wake_bake/js/main.js
@@ -172,6 +172,12 @@
 
   });
 
+  // -------------------------------Маска для телефона------------------------
+
+  const tellInputs = document.querySelectorAll('input[type="tel"]')
+  const im = new Inputmask('+7 (999) 999-99-99')
+  im.mask(tellInputs)
+
 })()
 
 
